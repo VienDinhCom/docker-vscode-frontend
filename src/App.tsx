cb1118d92533cb0d984b55d48f6fef616a4c227f
Vien Dinh <maxvien@gmail.com>
2025-04-30 16:57:47 +0700

    vite: call /api/hello in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('Hello from Front End!');
+
+  function hello() {
+    const baseUrl = import.meta.env.API_URL || 'https://localhost/api';
+
+    fetch(baseUrl + '/hello').then(async (res) => {
+      const data = await res.json();
+
+      setMessage(data.message);
+    });
+  }
 
   return (
     <>
@@ -16,20 +26,22 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>Docker VSCode</h1>
+        <p className="message">{message}</p>
+        <button onClick={hello}>Click Me</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+
+        <p className="read-the-docs">
+          <a href="https://github.com/VienDinhCom/docker-vscode-fullstack" target="_blank">
+            Learn More About Docker VSCode
+          </a>
+        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
